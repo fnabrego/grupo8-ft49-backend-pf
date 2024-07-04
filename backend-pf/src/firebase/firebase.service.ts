ce56d5d683d90bb1322bd9158e78e88499dd4df5
juampiramallo <juampi2005.ca@gmail.com>
2024-07-04 15:35:44 -0300

    firebase_mail_env arreglados

--- backend-pf/src/firebase/firebase.service.ts
+++ backend-pf/src/firebase/firebase.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
 
 @Injectable()
 export class FirebaseService {
-    async uploadFile(file): Promise<string>{
-        const storage = getStorage();
-        
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-        
-        const filePath = `${year}/${month}/${file.originalname}`;
-        const storageRef = ref(storage, filePath);
+  async uploadFile(file): Promise<string> {
+    const storage = getStorage();
 
-        try {
-            await uploadBytes(storageRef, file.buffer);
-            console.log('Archivo subido a la nube');
-            return 'Archivo subido exitosamente';
-        } catch (error) {
-            throw new Error(error);
-        }
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+
+    const filePath = `${year}/${month}/${file.originalname}`;
+    const storageRef = ref(storage, filePath);
+
+    try {
+      await uploadBytes(storageRef, file.buffer);
+      console.log('Archivo subido a la nube');
+      return 'Archivo subido exitosamente';
+    } catch (error: any) {
+      throw new Error(error);
     }
+  }
 }
