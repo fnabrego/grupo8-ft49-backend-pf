d6eb253f0b74b82b6ab18583607cb0d7d0b67b3d
Franco Abrego <f.n.abrego@outlook.com.ar>
2024-07-04 12:41:42 -0300

    Cambios para docker y deploy

--- backend-pf/src/config/typeorm.ts
+++ backend-pf/src/config/typeorm.ts
@@ -2,6 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 dotenvConfig({ path: '.env.development' });
+const isSSL = process.env.POSTGRES_SSL === 'true';
 
 const config = {
   type: 'postgres',
@@ -10,6 +11,18 @@ const config = {
   port: process.env.DB_PORT as unknown as number,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  ssl: isSSL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : undefined,
+  extra: {
+    ssl: isSSL
+      ? {
+          rejectUnauthorized: false,
+        }
+      : undefined,
+  },
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
