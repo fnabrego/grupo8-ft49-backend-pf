564d584f91d235026c5a089d2ba52c3f75a873f4
juampiramallo <juampi2005.ca@gmail.com>
2024-07-05 14:57:56 -0300

    Actualizado el return de  uploadImage

--- backend-pf/src/users/users.repository.ts
+++ backend-pf/src/users/users.repository.ts
@@ -37,7 +37,7 @@ export class UsersRepository {
     const user = await this.usersRepository.findOne({
       where: { id },
       relations: {
-        orders: {shipments: true}
+        orders: { shipments: true },
       },
     });
     if (!user)
